673a8882c1e25828857fd03f59b19647a4fec88b
zamboilles21 <zamboilles@turr.hu>
2022-11-16 10:06:10 +0100

    törlés

--- Mills/Mills/Game Area.Designer.cs
+++ Mills/Mills/Game Area.Designer.cs
@@ -123,6 +123,7 @@ namespace Mills
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(752, 559);
             this.Controls.Add(this.lbl_Player1_pieces);
             this.Controls.Add(this.lbl_Player2_pieces);
